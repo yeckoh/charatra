a7fb2b8382b9d08a071304c770551eba5fb589e8
yeckoh <yeckoh@gmail.com>
2020-02-08 15:26:00 -0500

    partial routine, how do we send a jaysun object for authserv to check a property?

--- frontend/src/app/shared/auth.service.ts
+++ frontend/src/app/shared/auth.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 // import 'rxjs/add/operator/map';
 
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
 import { JsonPipe } from '@angular/common';
 
@@ -31,7 +31,26 @@ export class AuthService {
 
     // post req to routes/adduser
     return this.http.post('http://localhost:3000/users/register', user, httpOptions).pipe(map(res => JSON.stringify(res)));
+    // .pipe(map(res => JSON.stringify(res))); returns a string, which happens to contain the object
+    // MAYBE we want the object so we use .pipe res => res
+  }
 
+  // auth a user
+  authenticateUser(user) {
+    // make a post req to authenticate
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+
+    // post req to routes/adduser
+    return this.http.post('http://localhost:3000/users/authenticate', user, httpOptions).pipe(map(res => JSON.stringify(res)));
+    // its gnona return: [if success] a token for users to store,
+    // and the user info, which we're gonna want to store that
   }
 
 
--- frontend/src/app/signin/signin.component.ts
+++ frontend/src/app/signin/signin.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, createPlatformFactory } from '@angular/core';
 
+import { AuthService } from '../shared/auth.service';
+import { Router } from '@angular/router'; // router to redirect
+import { FlashMessagesService } from 'angular2-flash-messages';
+
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
@@ -8,16 +12,34 @@ import { Component, OnInit, createPlatformFactory } from '@angular/core';
 export class SigninComponent implements OnInit {
 
   username: string;
-  email: string;
   password: string;
 
-  constructor() { }
+  // inject authservice as a dependency via constructor
+  // router and flashservice too
+  constructor(private authServ: AuthService,
+              private router: Router,
+              private flashMsg: FlashMessagesService) { }
 
   ngOnInit() {
   }
 
   onSigninSubmit() {
-    // do something
+    const user = { // make a new object
+      username: this.username,
+      password: this.password
+    };
+
+    // take object and submit it through auth service to the backend authenticate router
+    // uses authService
+    this.authServ.authenticateUser(user).subscribe(data => {
+      console.log(data); // should be no user found, wrong password, or token + user:_id
+      if (data) { // how do we 'blindly' check for a json object's property? we know success: exists but its just an object here
+        console.log('success goes here');
+      } else {
+        this.flashMsg.show('Invalid login credentials', {timeout: 3500});
+        this.router.navigate(['signin']);
+      }
+    });
   }
 
 
