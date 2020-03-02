ca20e90e1db015ebac86332ca7037246d92baccd
yeckoh <yeckoh@gmail.com>
2020-03-01 20:19:39 -0500

    sample charamaker, featurecard

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { DialogNewcharaComponent } from '../dialogs/dialog-newchara/dialog-newchara.component';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  constructor(public dialog: MatDialog) { }
 
+  public cardcontent = 'feature desription and stuff goes in here';
+  public cardtitle = 'sample_featurecard';
 
+  openDialog() {
+    // open accepts 2 params
+    // component, optional_configuration
+    this.dialog.open(DialogNewcharaComponent);
+  }
 }
+
+
+/// TODO:
+// make a button that makes a sample character with at least one of everything in the database
+// use ngOnChanges to update view when properties change
+// or just stick to two-way binding
