37a6223a1844bbf12bfa44bbfbec23de0b4bf161
Mrcoole7890 <kendallcole7@gmail.com>
2020-03-08 19:28:21 -0700

    added CRUD operations for the container model

--- models/containers.model.js
+++ models/containers.model.js
@@ -7,8 +7,17 @@ var ContainerSchema = mongoose.Schema({
     listof_equippeditems: [mongoose.Schema.Types.ObjectId],
     listof_unequippeditems: [mongoose.Schema.Types.ObjectId]
 });
-  
-  
+
+
 const Container = module.exports = mongoose.model('Containers', ContainerSchema);
   
 // schema model functions -> {mongoose functions}
+
+module.exports.SaveContainers = function(containersobj) {
+    containersobj.save();
+}
+
+module.exports.GetAllContainers = function(allids) {
+    var query = Container.find().where('_id').in(allids).exec();
+    return query;
+}
