a004c3abf7826e8a4ec0565a91bb4392dd442339
Mrcoole7890 <kendallcole7@gmail.com>
2020-03-10 10:16:10 -0700

    Added CRUD OPS for effects.model.js

--- models/effects.model.js
+++ models/effects.model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 var EffectSchema = mongoose.Schema({
     target: mongoose.Schema.Types.ObjectId,
-    
+
     value: String,
 
     addby: Boolean,
@@ -11,8 +11,18 @@ var EffectSchema = mongoose.Schema({
     minvalof: Boolean,
     basevalof: Boolean
 });
-  
-  
+
+
   const Effect = module.exports = mongoose.model('Effects', EffectSchema);
-  
+
   // schema model functions -> {mongoose functions}
+
+
+  module.exports.SaveEffect = function(effectobj) {
+      effectobj.save();
+  }
+
+  module.exports.GetAllEffect = function(allids) {
+      var query = Effect.find().where('_id').in(allids).exec();
+      return query;
+  }
