c0c7ea71e66d48bc9bc8a548410b199517e088b1
Mrcoole7890 <kendallcole7@gmail.com>
2020-03-14 15:13:13 -0700

    Added dynamic list for equpment input

--- frontend/src/app/equipment-list/equipment-list.component.ts
+++ frontend/src/app/equipment-list/equipment-list.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentListComponent implements OnInit {
 
+  equipmentList : String = ["GreatSword", "Health Vial", "Recall Potion"]; 
+
   constructor() { }
 
   ngOnInit() {
