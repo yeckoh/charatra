70877250aa0f978c57b38860f13a8643b3d1c80c
Mrcoole7890 <kendallcole7@gmail.com>
2020-03-22 11:31:59 -0700

    added textarea to dialog

--- frontend/src/app/dialog-persona/dialog-persona.component.ts
+++ frontend/src/app/dialog-persona/dialog-persona.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'app-dialog-persona',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogPersonaComponent implements OnInit {
 
-  constructor() { }
+  title: string;
+
+  constructor(@Inject(MAT_DIALOG_DATA) data) {
+    this.title = data.inPersonaDataType;
+  }
 
   ngOnInit() {
   }
