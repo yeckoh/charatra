952ecec93d5e9dd91164df327c104443fa1b183c
Mrcoole7890 <kendallcole7@gmail.com>
2020-03-23 08:48:19 -0700

    persona tab now displays the current persona data

--- frontend/src/app/chara/tabs/tab-persona/tab-persona.component.ts
+++ frontend/src/app/chara/tabs/tab-persona/tab-persona.component.ts
@@ -4,6 +4,10 @@ import { CharaService } from 'src/app/shared/chara.service';
 import { DialogPersonaComponent } from 'src/app/dialog-persona/dialog-persona.component';
 import { MatDialog } from '@angular/material';
 
+import { Chara } from 'src/app/shared/chara.model';
+import { CharaService } from 'src/app/shared/chara.service';
+import { Subscription } from 'rxjs';
+
 @Component({
   selector: 'app-tab-persona',
   templateUrl: './tab-persona.component.html',
@@ -12,6 +16,17 @@ import { MatDialog } from '@angular/material';
 export class TabPersonaComponent implements OnInit {
 
   constructor(private charaservice: CharaService, private personaDialog: MatDialog) { }
+
+  private name: string = this.charaservice.CharaSelected.persona.name;
+  private gender: string = this.charaservice.CharaSelected.persona.gender;
+  private description: string = this.charaservice.CharaSelected.persona.description;
+  private personality: string = this.charaservice.CharaSelected.persona.personality;
+  private ideals: string = this.charaservice.CharaSelected.persona.ideals;
+  private bonds: string = this.charaservice.CharaSelected.persona.bonds;
+  private race: string = this.charaservice.CharaSelected.persona.race;
+  private background: string = this.charaservice.CharaSelected.persona.background;
+
+  private subscriptions: Subscription;
 
   ngOnInit() {
   }
