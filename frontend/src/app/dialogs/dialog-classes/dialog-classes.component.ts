f6bc83cbc86203273ded2b1375f23580a39c8369
Mrcoole7890 <kendallcole7@gmail.com>
2020-03-24 18:49:49 -0700

    Added inputs for classes dialog

--- frontend/src/app/dialogs/dialog-classes/dialog-classes.component.ts
+++ frontend/src/app/dialogs/dialog-classes/dialog-classes.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
+import { CharaService } from 'src/app/shared/chara.service';
+import { Chara } from 'src/app/shared/chara.model';
 
 @Component({
   selector: 'app-dialog-classes',
   templateUrl: './dialog-classes.component.html',
   styleUrls: ['./dialog-classes.component.scss']
 })
+
 export class DialogClassesComponent implements OnInit {
 
-  constructor() { }
+  title: string;
+
+  constructor(@Inject(MAT_DIALOG_DATA) data) {
+    this.title = data.inPersonaDataType;
+  }
 
   ngOnInit() {
   }
+
+
 
 }
