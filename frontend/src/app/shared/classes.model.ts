f0cbc625d28d4e36be99dfcdcbbebf62e7762c8c
Mrcoole7890 <kendallcole7@gmail.com>
2020-03-24 19:06:07 -0700

    Modified classes.model.ts to reflect the database

--- frontend/src/app/shared/classes.model.ts
+++ frontend/src/app/shared/classes.model.ts
@@ -4,6 +4,6 @@ export class Classes {
   class_hitpoints: string;
   class_level: number;
   caster_level: string;
-  listof_classspell_list: [string];
-  listof_classfeatures: [string];
+  //listof_classspell_list: [string];
+  //listof_classfeatures: [string];
 }
