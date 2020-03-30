b7ac5c84e3468bd1a5d17134cb49883a3fecadd7
yeckoh <yeckoh@gmail.com>
2020-03-29 21:16:04 -0400

    fix classes.model again for some reason

--- frontend/src/app/shared/classes.model.ts
+++ frontend/src/app/shared/classes.model.ts
@@ -1,9 +1,10 @@
 export class Classes {
   // tslint:disable: variable-name
   _id: string;
+  class_name; string;
   class_hitpoints: string;
   class_level: number;
   caster_level: string;
-  //listof_classspell_list: [string];
-  //listof_classfeatures: [string];
+  // listof_classspell_list: [string];
+  // listof_classfeatures: [string];
 }
