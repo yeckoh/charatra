84b68eb772f1e085872f2ad24052839d1f23aa37
yeckoh <yeckoh@gmail.com>
2020-04-12 17:23:33 -0400

    spell cards. Mostly spell dialog. Spell delete. Spell attack & save create/del work. Drop some useless consolelogs

--- frontend/src/app/dialogs/dialog-spell/dialog-spell.component.ts
+++ frontend/src/app/dialogs/dialog-spell/dialog-spell.component.ts
@@ -5,6 +5,10 @@ import { ModifierPipe } from 'src/app/pipes/modifier.pipe';
 import { Chara } from 'src/app/shared/chara.model';
 import evaluate, { registerFunction } from 'ts-expression-evaluator';
 import { Subscription } from 'rxjs';
+import { Spells } from 'src/app/shared/spells.model';
+import { Classes } from 'src/app/shared/classes.model';
+import { DialogAttackComponent } from '../dialog-attack/dialog-attack.component';
+import { DialogSavingthrowComponent } from '../dialog-savingthrow/dialog-savingthrow.component';
 
 @Component({
   selector: 'app-dialog-spell',
@@ -18,7 +22,7 @@ export class DialogSpellComponent implements OnInit, OnDestroy {
               private thisDialog: MatDialogRef<DialogSpellComponent>,
               private subDialog: MatDialog,
               private modPipe: ModifierPipe) {
-      this.chara = data.chara as Chara;
+      this.chara = data.chara as Chara; // data: {selected_spell, chara: this.chara}});
 
       this.level = this.chara.chara_class.class_level;
       this.updateProf();
@@ -34,9 +38,11 @@ export class DialogSpellComponent implements OnInit, OnDestroy {
       this.intMod = this.modPipe.transform(this.int);
       this.wisMod = this.modPipe.transform(this.wis);
       this.chaMod = this.modPipe.transform(this.cha);
+      this.spell = data.selected_spell as Spells;
     }
 
     chara: Chara;
+    spell: Spells;
     // pulled straight
     private str = 0;
     private dex = 0;
@@ -71,6 +77,34 @@ export class DialogSpellComponent implements OnInit, OnDestroy {
     this.subscriptions.unsubscribe();
   }
 
+  ngOnInit() {
+    this.subscriptions = (this.charaservice.listenfor('Updated_one_chara').subscribe(data => {
+      // really we only need the stats for formula evaluation
+      this.chara = data as Chara;
+      this.level = this.chara.chara_class.class_level;
+      this.updateProf();
+      this.str = this.chara.stats.str;
+      this.dex = this.chara.stats.dex;
+      this.con = this.chara.stats.con;
+      this.int = this.chara.stats.int;
+      this.wis = this.chara.stats.wis;
+      this.cha = this.chara.stats.cha;
+      this.strMod = this.modPipe.transform(this.str);
+      this.dexMod = this.modPipe.transform(this.dex);
+      this.conMod = this.modPipe.transform(this.con);
+      this.intMod = this.modPipe.transform(this.int);
+      this.wisMod = this.modPipe.transform(this.wis);
+      this.chaMod = this.modPipe.transform(this.cha);
+    }));
+
+    this.subscriptions.add(this.charaservice.listenfor('Updated_selected_class').subscribe(data => {
+      this.chara.chara_class = data as Classes;
+      this.level = this.chara.chara_class.class_level;
+      this.updateProf();
+    }));
+  }
+
+
   updateProf() {
     this.profBonus = 1 + Math.ceil(this.level / 4); // evaluate cant use math atm. funcs so rest in rip for now
   }
@@ -95,25 +129,82 @@ export class DialogSpellComponent implements OnInit, OnDestroy {
     }
   }
 
-  ngOnInit() {
-    this.subscriptions = (this.charaservice.listenfor('Updated_one_chara').subscribe(data => {
-      // really we only need the stats for formula evaluation
-      this.chara = data as Chara;
-      this.level = this.chara.chara_class.class_level;
-      this.updateProf();
-      this.str = this.chara.stats.str;
-      this.dex = this.chara.stats.dex;
-      this.con = this.chara.stats.con;
-      this.int = this.chara.stats.int;
-      this.wis = this.chara.stats.wis;
-      this.cha = this.chara.stats.cha;
-      this.strMod = this.modPipe.transform(this.str);
-      this.dexMod = this.modPipe.transform(this.dex);
-      this.conMod = this.modPipe.transform(this.con);
-      this.intMod = this.modPipe.transform(this.int);
-      this.wisMod = this.modPipe.transform(this.wis);
-      this.chaMod = this.modPipe.transform(this.cha);
-    }));
+/*
+    // UPDATE_ONE
+    socket.on('Update_selected_spell', function(sent_in_data) {
+        // data is .spell obj .charaid
+    socket.on('Delete_selected_spell', function(sent_in_data) {
+        // data consists of .spellid .charaid
+*/
+
+
+  sendSpellDialogUpdate() {
+    const updateData = {
+      spell: this.spell,
+      charaid: this.charaservice.CharaId
+    };
+    this.charaservice.sendback('Update_selected_spell', updateData);
+  }
+
+  sendSpellDelete() {
+    const spellAndcharaId = {
+      spellid: this.spell._id,
+      charaid: this.charaservice.CharaId
+    };
+    this.charaservice.sendback('Delete_selected_spell', spellAndcharaId);
+    this.thisDialog.close();
+  }
+
+/*
+
+  sendItemDelete() {
+
+    const itemAndUserId = {
+      charaid: this.charaservice.CharaId,
+      itemid: this.thisItem._id,
+      parentid: undefined
+    };
+    switch (this.whichContainer) {
+      case 'inventory':
+        itemAndUserId.parentid = this.inventory._id;
+        break;
+      case 'carried':
+        itemAndUserId.parentid = this.carried._id;
+        break;
+      case 'extra':
+        itemAndUserId.parentid = this.extra._id;
+        break;
+    }
+    this.charaservice.sendback('Delete_selected_item', itemAndUserId);
+    this.thisDialog.close();
+  }
+
+*/
+
+
+  newAttack() {
+    const forwardingdata = {
+      spell_id: this.spell._id,
+      chara_id: this.charaservice.CharaId
+    };
+    this.charaservice.sendback('Make_new_attack', forwardingdata);
+  }
+
+  newSave() {
+    const forwardingdata = {
+      spell_id: this.spell._id,
+      chara_id: this.charaservice.CharaId
+    };
+    this.charaservice.sendback('Make_new_save', forwardingdata);
+  }
+
+  // tslint:disable: variable-name
+  openAttackDialog(selected_attack) {
+    this.subDialog.open(DialogAttackComponent, {data: selected_attack});
+  }
+
+  openSaveDialog(selected_save) {
+    this.subDialog.open(DialogSavingthrowComponent, {data: selected_save});
   }
 
 }
